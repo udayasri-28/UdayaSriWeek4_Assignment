f392c5c0d081355a96e61b108a91ecb8f2214571
udayasri-28 <udayasri682@gmail.com>
2026-04-07 15:09:25 +0530

    Added

---
+++ April06Assignments/WebApplication1/WebApplication1/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Person> Persons { get; set; } = null!;
+
+    }
+}
---
+++ April06Assignments/WebApplication1/WebApplication1/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }        // PK
+        public string Name { get; set; } = null!;
+        public int Age { get; set; }
+        public string City { get; set; } = null!;
+    }
+}
